b102309c3c8cb7db34c812ce256c6c2817c6d9b1
lollipoperzzz <56427940+lollipoperzzz@users.noreply.github.com>
2021-07-01 12:57:58 +0300

    Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './../App.css'
+
+function Header(){
+    return(
+        <div className='header'>
+            <img className='App-logo' src="https://cryptologos.cc/logos/aave-aave-logo.png" alt="no-logo"/>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+
+function Profile() {
+    return (
+        <div className='content'>
+            <img className='upper-image' src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="no-image"/>
+            <div>
+                ava+desc
+            </div>
+            <div>
+                MyPosts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
